efb1053e09f710cdb74f5f0c5bfa11421c862c31
vivianx9 <vivianx5085@gmail.com>
2024-05-29 11:01:08 -0400

    Day 10
    I finished up the blocking and attacking mechanism and tested it with some friends. Then I added an end screen so the game ends when a player loses. Lastly I added more instructions to the start screen to make it fit the game.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Times New Roman', 15)
 my_font1 = pygame.font.SysFont('Times New Roman', 30)
+my_font2 = pygame.font.SysFont('Times New Roman', 20)
 pygame.display.set_caption("Street Fighter")
 
 # set up variables for the display
@@ -18,9 +19,13 @@ bg = pygame.image.load("images/background.png")
 bg = pygame.transform.scale(bg, (1024, 768))
 
 welcome_message = "Welcome to Street Fighter"
-instructions_message = "Use WASD to move. Ability keys are E and Q"
+instructions_message = "Player One: Use AD to move. E is attack and Q is block."
+instructions_message2 = "Player Two: Use LEFT and RIGHT arrows to move. UP arrow to attack and DOWN arrow to block."
 character_instructions1 = "Choose a character for player 1"
 character_instructions2 = "Choose a character for player 2"
+
+winning_message = "Game over! The winner is"
+winner = "player"
 
 kirby = Kirby(200, 600)
 mario = Mario(300, 600)
@@ -32,8 +37,11 @@ game_start = False
 fight_start = False
 p1_chosen = False
 characters_determined = False
+game_end = False
 
 attack = False
+p1_block = False
+p2_block = False
 
 p1_c1 = False
 p1_c2 = False
@@ -45,9 +53,11 @@ p2_health = 100
 
 # render the text for later
 display_welcome_message = my_font1.render(welcome_message, True, (255, 255, 255))
-display_instructions_message = my_font1.render(instructions_message, True, (255, 255, 255))
+display_instructions_message = my_font2.render(instructions_message, True, (255, 255, 255))
+display_instructions2_message = my_font2.render(instructions_message2, True, (255, 255, 255))
 display_character_instructions1 = my_font1.render(character_instructions1, True, (255, 255, 255))
 display_character_instructions2 = my_font1.render(character_instructions2, True, (255, 255, 255))
+display_winning_message = my_font1.render(winning_message, True, (255, 255, 255))
 
 
 run = True
@@ -75,26 +85,41 @@ while run:
         # attacking and blocking
         # player one block
         if keys[pygame.K_q]:
-            p1.ability("block")
+            # p1.ability("block")
+            p1_block = True
+        else:
+            p1_block = False
+
         # player two block
-        if keys[pygame.K_2]:
-            p2.ability("block")
+        if keys[pygame.K_DOWN]:
+            # p2.ability("block")
+            p2_block = True
+        else:
+            p2_block = False
 
         if attack is False:
             # player one attack
             if keys[pygame.K_e]:
-                if p1.rect.colliderect(p2.rect) and p2.block is False:
+                if p1.rect.colliderect(p2.rect) and p2_block is False:
                     p2_health -= 25
                     attack = True
             # player two attack
-            if keys[pygame.K_1]:
-                if p2.rect.colliderect(p1.rect) and p1.block is False:
+            if keys[pygame.K_UP]:
+                if p2.rect.colliderect(p1.rect) and p1_block is False:
                     p1_health -= 25
                     attack = True
 
         if attack:
             pygame.time.wait(1000)
             attack = False
+
+        if p1_health == 0 or p2_health == 0:
+            game_end = True
+            if p1_health == 0:
+                winner = "Player Two"
+            if p2_health == 0:
+                winner = "Player One"
+            display_winner = my_font1.render(winner, True, (255, 255, 255))
 
 
     # character health bar
@@ -150,24 +175,31 @@ while run:
 
     # blitting
     # screen.fill((100, 100, 100))
-    if game_start is False:
-        screen.blit(display_welcome_message, (350, 300))
-        screen.blit(display_instructions_message, (300, 350))
-    else:
-        if fight_start is False:
-            screen.fill((100, 100, 100))
-            screen.blit(kirby.image, (200, 600))
-            screen.blit(mario.image, (350, 600))
-            if p1_chosen is False:
-                screen.blit(display_character_instructions1, (300, 200))
-            else:
-                screen.blit(display_character_instructions2, (300, 200))
+    if game_end is False:
+        if game_start is False:
+            screen.blit(display_welcome_message, (350, 300))
+            screen.blit(display_instructions_message, (200, 350))
+            screen.blit(display_instructions2_message, (50, 400))
         else:
-            screen.blit(bg, (0, 0))
-            screen.blit(p1_bar.image, (p1_bar.x, p1_bar.y))
-            screen.blit(p2_bar.image, (p2_bar.x, p2_bar.y))
-            screen.blit(p1.image, (p1.x, p1.y))
-            screen.blit(p2.image, (p2.x, p2.y))
+            if fight_start is False:
+                screen.fill((100, 100, 100))
+                screen.blit(kirby.image, (200, 600))
+                screen.blit(mario.image, (350, 600))
+                if p1_chosen is False:
+                    screen.blit(display_character_instructions1, (300, 200))
+                else:
+                    screen.blit(display_character_instructions2, (300, 200))
+            else:
+                screen.blit(bg, (0, 0))
+                screen.blit(p1_bar.image, (p1_bar.x, p1_bar.y))
+                screen.blit(p2_bar.image, (p2_bar.x, p2_bar.y))
+                screen.blit(p1.image, (p1.x, p1.y))
+                screen.blit(p2.image, (p2.x, p2.y))
+    else:
+        screen.fill((100, 100, 100))
+        screen.blit(display_winning_message, (300, 200))
+        screen.blit(display_winner, (400, 300))
+
     pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
--- mario.py
+++ mario.py
@@ -10,21 +10,16 @@ class Mario:
         self.image = pygame.transform.scale(self.image, (150, 150))
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        self.delta = 1
-        if self.x < 100:
-            self.x = 100
-        if self.x > 800:
-            self.x = 800
-        self.block = False
+        self.delta = 2
 
     def move_direction(self, direction):
         if direction == "right":
             self.x = self.x + self.delta
         if direction == "left":
             self.x = self.x - self.delta
+
+        if self.x < 100:
+            self.x = 100
+        if self.x > 800:
+            self.x = 800
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-    def ability(self, ability):
-        if ability == "block":
-            self.block = True
-
